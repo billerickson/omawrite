55b5e7bef284ec0f75b1fb61bb10d95a2814789e
Bill Erickson <bill@cultivatewp.com>
2026-09-25 09:40:44 -0500

    Remove extra spacing from copied paragraphs

--- src/backend.cpp
+++ src/backend.cpp
@@ -313,7 +313,7 @@ void Backend::copyAsHtml() {
 
     QString html = rendered.toHtml();
     static const QRegularExpression paragraphTag(QStringLiteral("<p(?:\\s+[^>]*)?>"));
-    html.replace(paragraphTag, QStringLiteral("<p style=\"margin: 0 0 1em 0;\">"));
+    html.replace(paragraphTag, QStringLiteral("<p style=\"margin: 0;\">"));
 
     auto *mimeData = new QMimeData;
     mimeData->setHtml(html);
